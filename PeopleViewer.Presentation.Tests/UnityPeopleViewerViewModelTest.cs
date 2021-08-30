951a3f9b609dc335329add8a207fd8bebe52cc11
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-08-30 15:29:22 -0500

    M05-06 - Add Unity container to unit tests.

---
+++ PeopleViewer.Presentation.Tests/UnityPeopleViewerViewModelTest.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PeopleViewer.SharedObjects;
+using PersonRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// 08/30/2021 03:04 pm - SSN - [20210830-1352] - [001] - M05-06 - Demo: Unit testing with a container
+// Copied from PeopleViewViewModelTest.cs
+
+namespace PeopleViewer.Presentation.Tests
+{
+
+    [TestClass]
+    public class UnityPeopleViewerViewModelTest
+    {
+
+        // IPersonRepository _repository;
+        IUnityContainer Container;
+
+        int numberOfTestRecords;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var people = new List<Person>()
+            {
+
+                new Person()
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    StartDate = new DateTime(2000, 10, 1),
+                    Rating = 7
+                },
+                new Person()
+                {
+                    FirstName = "Mary",
+                    LastName = "Thomas",
+                    StartDate = new DateTime(1971,7,23),
+                    Rating = 9
+                }
+            };
+
+            numberOfTestRecords = people.Count();
+
+            var repoMock = new Mock<IPersonRepository>();
+            repoMock.Setup(r => r.GetPeople()).Returns(people);
+
+            // _repository = repoMock.Object;
+            Container = new UnityContainer();
+            Container.RegisterInstance<IPersonRepository>(repoMock.Object);
+
+        }
+
+        [TestMethod]
+        [TestCategory("Unity")]
+        public void UnityPeople_OnRefreshCommand_IsPopulated()
+        {
+
+            // Arrange
+
+            // var vm = new PeopleViewerViewModel(_repository);
+            var vm = Container.Resolve<PeopleViewerViewModel>();
+
+            // Act
+
+            vm.RefreshPeopleCommand.Execute(null);
+
+
+            // Assert
+
+            Assert.IsNotNull(vm.People);
+            Assert.AreEqual(numberOfTestRecords, vm.People.Count());
+
+        }
+
+
+        [TestMethod]
+        [TestCategory("Unity")]
+        public void UnityPeople_OnClearCommand_IsEmpty()
+        {
+
+
+            // Arrange
+
+            // var vm = new PeopleViewerViewModel(_repository);
+            var vm = Container.Resolve<PeopleViewerViewModel>();
+
+
+            vm.RefreshPeopleCommand.Execute(null);
+            Assert.AreEqual(numberOfTestRecords, vm.People.Count(), "Invalid test arrangement setup (20210830-1002)");
+
+            // Act
+
+            vm.ClearPeopleCommand.Execute(null);
+
+            // Assert
+
+            Assert.AreEqual(0, vm.People.Count());
+
+        }
+
+
+    }
+}
---
+++ PersonRepository.Service.Tests/UnityServiceRepositoryTest.cs
@@ -0,0 +1,151 @@
+﻿
+
+// 08/30/2021 03:14 pm - SSN - [20210830-1352] - [002] - M05-06 - Demo: Unit testing with a container
+
+
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PeopleViewer.SharedObjects;
+using PersonRepository.Service.MyService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonRepository.Service.Tests
+{
+
+    [TestClass]
+    public class UnityServiceRepositoryTest
+    {
+
+        // IPersonService _service;
+        IUnityContainer Container;
+
+        int numberOfTestRecords;
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var people = new List<Person>()
+            {
+
+                new Person()
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    StartDate = new DateTime(2000, 10, 1),
+                    Rating = 7
+                },
+                new Person()
+                {
+                    FirstName = "Mary",
+                    LastName = "Thomas",
+                    StartDate = new DateTime(1971,7,23),
+                    Rating = 9
+                }
+            };
+
+            numberOfTestRecords = people.Count();
+
+            var svcMock = new Mock<IPersonService>();
+            svcMock.Setup(r => r.GetPeople()).Returns(people);
+            svcMock.Setup(r => r.GetPerson(It.IsAny<string>())).Returns((string n) => people.FirstOrDefault(p => p.LastName == n));
+
+            // _service = svcMock.Object;
+            Container = new UnityContainer();
+            Container.RegisterInstance<IPersonService>(svcMock.Object);
+            Container.RegisterType<ServiceRepository>(new InjectionProperty("ServiceProxy"));
+        }
+
+
+        [TestMethod]
+        [TestCategory("Unity")]
+        public void UnityGetPeople_OnExecute_ReturnsPeople()
+        {
+
+            // Arrange
+
+            // var repo = new ServiceRepository();
+            var repo = Container.Resolve<ServiceRepository>();
+
+            // repo.ServiceProxy = _service;
+            // No longer needed.
+
+
+            // Act
+
+            var output = repo.GetPeople();
+
+
+            // Assert
+
+            Assert.IsNotNull(output);
+            Assert.AreEqual(numberOfTestRecords, output.Count());
+        }
+
+
+        [TestMethod]
+        [TestCategory("Unity")]
+        public void UnityGetPerson_OnExecuteWithValidValue_ReturnsPerson()
+        {
+
+
+            // Arrange
+
+            //var repo = new ServiceRepository();
+            var repo = Container.Resolve<ServiceRepository>();
+
+            // repo.ServiceProxy = _service;
+            // No longer needed.
+
+
+            string expectedLastName = "Smith";
+
+            // Act
+
+            var output = repo.GetPerson(expectedLastName);
+
+
+            // Assert
+
+            Assert.IsNotNull(output);
+            Assert.AreEqual(expectedLastName, output.LastName);
+
+
+        }
+
+
+
+        [TestMethod]
+        [TestCategory("Unity")]
+        public void UnityGetPerson_OnExecuteWithInvalidValue_ReturnsNull()
+        {
+
+
+            // Arrange
+
+            // var repo = new ServiceRepository();
+            var repo = Container.Resolve<ServiceRepository>();
+
+            // repo.ServiceProxy = _service;
+            // No longer needed.
+
+
+            // Act
+
+            var output = repo.GetPerson("InvalidLastName");
+
+
+            // Assert
+
+            Assert.IsNull(output);
+
+
+        }
+
+
+
+    }
+}

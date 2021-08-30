ea6c97a76b9965e16c7766b38f14d27fe0ae346d
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-08-30 10:05:24 -0500

    M04-03 - Added short unit test with mocking. MSTest + Moq.

---
+++ PeopleViewer.Presentation.Tests/PeopleViewerViewModelTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PeopleViewer.SharedObjects;
+using PersonRepository.Interface;
+
+// 08/30/2021 09:44 am - SSN - [20210830-0944] - [001] -  M04-03 - Demo: Unit testing 
+
+namespace PeopleViewer.Presentation.Tests
+{
+    [TestClass]
+    public class PeopleViewerViewModelTest
+    {
+        IPersonRepository _repository;
+        int numberOfTestRecords;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var people = new List<Person>()
+            {
+
+                new Person()
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    StartDate = new DateTime(2000, 10, 1),
+                    Rating = 7
+                },
+                new Person()
+                {
+                    FirstName = "Mary",
+                    LastName = "Thomas",
+                    StartDate = new DateTime(1971,7,23),
+                    Rating = 9
+                }
+            };
+
+            numberOfTestRecords = people.Count();
+
+            var repoMock = new Mock<IPersonRepository>();
+            repoMock.Setup(r => r.GetPeople()).Returns(people);
+
+            _repository = repoMock.Object;
+
+        }
+
+        [TestMethod]
+        public void People_OnRefreshCommand_IsPopulated()
+        {
+
+            // Arrange
+
+            var vm = new PeopleViewerViewModel(_repository);
+
+
+            // Act
+
+            vm.RefreshPeopleCommand.Execute(null);
+
+
+            // Assert
+
+            Assert.IsNotNull(vm.People);
+            Assert.AreEqual(numberOfTestRecords, vm.People.Count());
+
+        }
+
+
+        [TestMethod]
+        public void People_OnClearCommand_IsEmpty()
+        {
+
+
+            // Arrange
+
+            var vm = new PeopleViewerViewModel(_repository);
+            vm.RefreshPeopleCommand.Execute(null);
+            Assert.AreEqual(numberOfTestRecords, vm.People.Count(), "Invalid test arrangement setup (20210830-1002)");
+
+            // Act
+
+            vm.ClearPeopleCommand.Execute(null);
+
+            // Assert
+
+            Assert.AreEqual(0, vm.People.Count());
+
+        }
+
+
+    }
+}
---
+++ PeopleViewer.Presentation.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PeopleViewer.Presentation.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PeopleViewer.Presentation.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0d8aedf-da3e-49ab-af33-6737616bc2e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
